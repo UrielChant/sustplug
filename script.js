641051d61806246e03559a23aa368c79bc1c34df
UrielChant <147094967+UrielChant@users.noreply.github.com>
2024-01-29 16:23:59 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function search() {
+    var input = document.getElementById('search-input').value;
+    document.getElementById('results').innerHTML = 'Resultados para "' + input + '"';
+
+    // Aquí puedes agregar la lógica para filtrar tus datos o realizar una solicitud a un servidor
+}
+// Obtén el modal
+var modal = document.getElementById('myModal');
+
+// Obtén la imagen e inserta dentro del modal - usa su "alt" como texto
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+
+// Obtén el elemento <span> que cierra el modal
+var span = document.getElementsByClassName("close")[0];
+
+// Cuando el usuario haga clic en <span> (x), cierra el modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
